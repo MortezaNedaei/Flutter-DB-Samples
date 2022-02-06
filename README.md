367c4bbc3d430146ccff573f9e66ebd9e65a47b2
Morteza Nedaei <nedaei1375@gmail.com>
2022-02-06 18:01:23 +0330

    initialize project

---
+++ README.md
@@ -0,0 +1,5 @@
+# Flutter DB Samples
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
---
+++ android/app/src/main/kotlin/com/mone/flutter_db/flutter_db/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.mone.flutter_db.flutter_db
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
