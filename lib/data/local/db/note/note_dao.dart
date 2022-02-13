443658b155361be61571eaa329f0210deed084d8
Morteza Nedaei <nedaei1375@gmail.com>
2022-02-13 13:22:40 +0330

    add dao for communicate between db entity and repository

--- lib/data/local/db/note/note_dao.dart
+++ lib/data/local/db/note/note_dao.dart
@@ -1,9 +1,13 @@
 import 'package:flutter_db/data/local/db/note/note_entity.dart';
 
 abstract class NoteDao {
-  Future<int> insert(Map<String, dynamic> row);
+  Future<int> insert(NoteEntity note);
+
+  Future<List<int>> insertAll(List<NoteEntity> notes);
 
   Future<List<NoteEntity>> getAll();
+
+  Future<List<NoteEntity?>> getMany(List<int> ids);
 
   Future<List<NoteEntity>> search(String query);
 
@@ -11,5 +15,7 @@ abstract class NoteDao {
 
   Future<int> update(NoteEntity note);
 
-  Future<int> delete(int id);
+  Future<bool> delete(int id);
+
+  Future<bool> deleteAll();
 }
