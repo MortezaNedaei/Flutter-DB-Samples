70bdd53c94b2f0b5ec119643ab2aeb55db1e84f2
Morteza Nedaei <nedaei1375@gmail.com>
2022-02-13 13:26:44 +0330

    use database helper for create and retrieve objectbox store

--- lib/data/local/db/db_helper.dart
+++ lib/data/local/db/db_helper.dart
@@ -1,66 +1,46 @@
 import 'dart:io';
-import 'package:flutter_db/data/local/db/note/note_entity.dart';
+
+import 'package:flutter_db/objectbox.g.dart';
+import 'package:objectbox/objectbox.dart';
 import 'package:path/path.dart';
-import 'package:sqflite/sqflite.dart';
-import 'package:sqflite/utils/utils.dart';
+import 'package:path_provider/path_provider.dart';
 
 class DatabaseHelper {
   static const _databaseName = "todo.db";
-  static const _databaseVersion = 1;
-  static Database? _database;
+
+  Store? _store;
 
   /// private constructor
   DatabaseHelper._privateConstructor();
+
   static final DatabaseHelper instance = DatabaseHelper._privateConstructor();
 
   /// asking for a database
-  Future<Database?> get database async {
-    if (_database != null) return _database;
+  Future<Store> get store async {
+    if (_store != null) return _store!;
 
     // create a database if one doesn't exist
-    _database = await _initDatabase();
-    return _database;
+    _store = await _initDatabase();
+    return _store!;
   }
 
-  /// function to return a database
+  /// function to return a store
   _initDatabase() async {
-    String path = join(await getDatabasesPath(), _databaseName);
-    return await openDatabase(path,
-        version: _databaseVersion, onCreate: _onCreate);
+    Directory directory = await getApplicationDocumentsDirectory();
+    _store = Store(
+      getObjectBoxModel(),
+      directory: join(directory.path, _databaseName),
+    );
+
+    _onCreate(_store!);
+    return _store;
   }
 
   /// create a database since it doesn't exist
-  Future _onCreate(Database db, int version) async {
-    // sql code
-    await db.execute('''
-      CREATE TABLE ${NoteEntity.tableName} (
-        ${NoteEntity.columnID} INTEGER PRIMARY KEY,
-        ${NoteEntity.columnName} TEXT NOT NULL,
-        ${NoteEntity.columnDescription} TEXT NOT NULL,
-        ${NoteEntity.columnTimestamp} INTEGER NOT NULL
-      );
-      ''');
+  Future _onCreate(Store store) async {
+    // additional implementations
   }
 
   /// close db connection
-  Future close() async => _database?.close();
-
-  /// Check if a table exists
-  Future<bool> tableExists(DatabaseExecutor db, String table) async {
-    var count = firstIntValue(await db.query('sqlite_master',
-        columns: ['COUNT(*)'],
-        where: 'type = ? AND name = ?',
-        whereArgs: ['table', table]));
-    return count! > 0;
-  }
-
-  /// List table names
-  Future<List<String>> getTableNames(DatabaseExecutor db) async {
-    var tableNames = (await db
-            .query('sqlite_master', where: 'type = ?', whereArgs: ['table']))
-        .map((row) => row['name'] as String)
-        .toList(growable: false)
-      ..sort();
-    return tableNames;
-  }
+  Future close() async => _store?.close();
 }
